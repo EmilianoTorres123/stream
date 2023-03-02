6c4c4a647b1673d17c4f2d9e9b1a83a49737760d
emiliano <106941242+EmilianoTorres123@users.noreply.github.com>
2023-03-02 11:43:04 -0600

    master add app_netflix

--- cache.py
+++ cache.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import streamlit as st
-
-name_link ='dataset.csv'
+import codecs
+name_link =codec.open('https://raw.githubusercontent.com/adsoftsito/nosql/main/csv/movies.csv','ru','latin1')
 
 st.title('Streamlit con cache')
 
 @st.cache
 def load_data(nrows):
     data = pd.read_csv(name_link, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
     return data
 
 data_load_state = st.text('cargando')
